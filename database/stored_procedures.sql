776bcc06452530d295825c0aaef5558881773c12
Facundo Carballo <carballofacundo70@gmail.com>
2024-01-12 20:04:36 -0300

    sp update

--- database/stored_procedures.sql
+++ database/stored_procedures.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE DOCKER_TODO_LIST;
 USE DOCKER_TODO_LIST;
 
+DROP PROCEDURE CreateTask;
+
 DELIMITER //
-CREATE PROCEDURE CreateTask(IN description VARCHAR(255))
-BEGIN
-	INSERT INTO Task (description) VALUES (description);
-END //
+	CREATE PROCEDURE CreateTask(IN description VARCHAR(255), OUT taskId INT,  OUT createdTimestamp TIMESTAMP)
+	BEGIN
+		INSERT INTO Task (description) VALUES (description);
+		SELECT LAST_INSERT_ID() INTO taskId;
+		SELECT created_at INTO createdTimestamp FROM Task WHERE id = taskId;
+	END //
 DELIMITER ;
